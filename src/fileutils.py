f8835ae175021689d6a3bb581567c336cb71b85f
Zoltán M <ego.pressure@gmail.com>
2026-03-04 12:25:50 +0100

    Load file from relative path

--- src/fileutils.py
+++ src/fileutils.py
@@ -4,9 +4,8 @@ from pathlib import Path
 
 def get_file_path(filename: str) -> str:
     current_path = Path(__file__).resolve().parent
-    target_path = Path(r"C:\DEV\sim-stock\sim-stock-be\src")
 
-    if current_path == target_path:
+    if str(current_path).endswith('src'):
         data_path = current_path / filename
         logging.warning(f"data_path for file loading: {data_path}")
         return str(data_path)
