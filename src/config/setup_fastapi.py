75db739394db198a40c9c8e32cdc7ac6cde24e22
Zoltán M <ego.pressure@gmail.com>
2026-03-04 11:53:06 +0100

    CORS issue

--- src/config/setup_fastapi.py
+++ src/config/setup_fastapi.py
@@ -6,8 +6,12 @@ def setup_fastapi() -> FastAPI:
     app = FastAPI()
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=False,
+        allow_origins=[
+            "http://localhost:3000",
+            "https://sim-stock-be.vercel.app/",
+            "https://sim-stock-fe.vercel.app/",
+        ],
+        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
